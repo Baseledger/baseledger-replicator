ce2736eab2c64a945cbb021e7c9d65bfcd4a02aa
Jovan Milovanovic <jovan.milovanovic@finspot.rs>
2022-10-24 16:31:03 +0200

    add swagger UI

--- Program.cs
+++ Program.cs
@@ -1,11 +1,18 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "baseledger-replicator-api", Version = "v1" });
+
+    // TODO: After implementing JWT, add security definition
+});
 
 var app = builder.Build();
 
@@ -13,7 +20,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+        {
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "baseledger-replicator-api v1");
+            c.RoutePrefix = string.Empty;
+        });
 }
 
 app.UseHttpsRedirection();
