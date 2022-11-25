f9c8a82e0735de87acd8834a928cd709a714a645
kurticognjen <kurticognjen@gmail.com>
2022-11-25 15:29:00 +0100

    Add inital integration setup with an raw instruction set

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -31,7 +31,6 @@ public class AccountController : BaseController
     /// <response code="200">Successful registration</response>
     /// <response code="400">If an error occured during registration</response>
     [HttpPost("register")]
-    [Authorize]
     [ProducesResponseType((int)HttpStatusCode.OK)]
     [ProducesResponseType((int)HttpStatusCode.BadRequest, Type = typeof(string[]))]
     public async Task<IActionResult> RegisterUserAsync([FromBody] RegisterUserDto registerUserDto)
