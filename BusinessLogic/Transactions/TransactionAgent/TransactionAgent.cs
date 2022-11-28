a83b35e81566fd79999815ddabb9a69127db6a8a
Jovan Milovanovic <jovan.milovanovic@finspot.rs>
2022-11-28 21:35:48 +0100

    create transaction in replicator node

--- BusinessLogic/Transactions/TransactionAgent/TransactionAgent.cs
+++ BusinessLogic/Transactions/TransactionAgent/TransactionAgent.cs
@@ -1,7 +1,11 @@
+using Newtonsoft.Json;
+
 namespace baseledger_replicator.BusinessLogic.Transactions.TransactionAgent;
 
 public class TransactionAgent : ITransactionAgent
 {
+    // TODO: Should we be thinking about extracting urls and accessing them via options or something like that?
+    // Should we be introducing replicatorService for direct communication with replicator node or it's fine to invoke from the agent?
     private readonly string url = "http://0.0.0.0:1317/cosmos/tx/v1beta1/txs/";
 
     private readonly ILogger<TransactionAgent> _logger;
@@ -24,6 +28,7 @@ public class TransactionAgent : ITransactionAgent
             response.EnsureSuccessStatusCode();
             var responseString = response.Content.ReadAsStringAsync().Result;
             // TODO: add response body deserialization
+            // string something = JsonConvert.DeserializeObject<dynamic>(responseStr).property;
         }
         catch (Exception ex)
         {
@@ -31,5 +36,32 @@ public class TransactionAgent : ITransactionAgent
         }
 
         return true;
+    }
+
+    public async Task<string> CreateTransaction(Guid transactionId, string payload)
+    {
+        var httpClient = new HttpClient();
+        var uri = new Uri(this.url + "signAndBroadcast");
+        var body = new
+        {
+            transaction_id = transactionId,
+            payload = payload
+        };
+
+        HttpResponseMessage response;
+
+        try
+        {
+            response = await httpClient.PostAsJsonAsync(uri, body);
+            response.EnsureSuccessStatusCode();
+        }
+        catch (HttpRequestException ex)
+        {
+            _logger.LogError($"Error in creating transaction: {ex.Message} for transactionId: {transactionId}");
+            // TODO: How do we want to process errors and return them to the API caller?
+            return null;
+        }
+
+        return response.Content.ReadAsStringAsync().Result;
     }
 }
--- Controllers/TransactionController.cs
+++ Controllers/TransactionController.cs
@@ -1,4 +1,6 @@
+using baseledger_replicator.BusinessLogic.Transactions.Commands;
 using baseledger_replicator.BusinessLogic.Transactions.Queries;
+using baseledger_replicator.DTOs.Transactions;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -23,5 +25,23 @@ public class TransactionController : BaseController
     {
         var result = await Mediator.Send(new GetTransactionByTxHashQuery { TxHash = txHash });
         return Ok(result);
+    }
+
+    /// <summary>
+    /// Creates a transaction in the replicator node
+    /// </summary>
+    /// <param name="body">CreateTransactionDto containing transaction ID and payload</param>
+    [HttpPost]
+    [Route("create")]
+    public async Task<ActionResult> CreateTransactionInReplicatorNode([FromBody] CreateTransactionDto body)
+    {
+        var txHash = await Mediator.Send(new CreateTransactionInReplicatorNodeCommand
+        {
+            TransactionId = body.TransactionId,
+            Payload = body.Payload
+        });
+
+        // TODO: How and in what detail do we want to process errors and return them to the API caller?
+        return txHash != null ? Ok(txHash) : Ok("Error when creating transaction in replicator node.");
     }
 }
